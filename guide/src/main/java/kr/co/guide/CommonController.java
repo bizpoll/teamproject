060d17f4c29f5bf9defbb37fa8c7596e28297533
test <test@gmail.com>
2022-02-18 03:17:00 +0900

    프로젝트 시작 원본

--- guide/src/main/java/kr/co/guide/CommonController.java
+++ guide/src/main/java/kr/co/guide/CommonController.java
@@ -1,5 +1,24 @@
 package kr.co.guide;
 
-public class CommonController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String mainGet() {
+		log.info("==================== main get ====================");
+		
+	
+		return "common/sample";
+	}
 }
--- guide/src/main/webapp/WEB-INF/views/common/sample.jsp
+++ guide/src/main/webapp/WEB-INF/views/common/sample.jsp
@@ -3,20 +3,19 @@
 
 
 	
-	<%@ include file="../common/cssLink.jsp"%>
-	<%@ include file="../common/mainheader.jsp"%>
+<%@ include file="../common/cssLink.jsp"%>
+<%@ include file="../common/mainheader.jsp"%>
 	
 
-	<%-- 바디 시작 --%>
-	<body>
-		
-	<%-- 이곳에 작성해주세요 --%>
+<%-- 바디 시작 --%>
+<body>
 	
+<%-- 이곳에 작성해주세요 --%>
 	
-	<%-- 바디 끝 --%>
+<%-- 바디 끝 --%>
 
 	
-	<%@ include file="../common/jsLink.jsp"%>
-	<%@ include file="../common/mainfooter.jsp"%>
+<%@ include file="../common/jsLink.jsp"%>
+<%@ include file="../common/mainfooter.jsp"%>
 	
-	</body>
+</body>
